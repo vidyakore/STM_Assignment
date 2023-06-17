35ac22b7c1c8f1e2bc7a3df55bbf05a311f576f6
Vidya Maruti Kore <vidya.maruti.kore@sap.com>
2023-06-17 14:03:47 +0530

    added more test cases

--- top10WebScrape.js
+++ top10WebScrape.js
@@ -1,42 +1,94 @@
 const { Builder, By, Key } = require('selenium-webdriver');
 const fs = require('fs');
 
-async function scrapeSearchResults(keyword) {
+async function scrapeSearchResults() {
   const driver = await new Builder().forBrowser('chrome').build();
 
   try {
     await driver.get('https://www.google.com');
     await driver.manage().window().maximize();
 
-    const searchBox = await driver.findElement(By.className('gLFyf'));
-    // const searchBox = await driver.findElement(By.xpath('//*[@id="input"]'));
-    await searchBox.sendKeys(keyword, Key.RETURN);
-    await driver.sleep(2000); // Introduce a 2-second delay
+    //Test case 1 : 10 links are found for given keyword
+    try {
+      let searchBox = await driver.findElement(By.name('q'));
+      let keyword = 'cloud computing';
+      await searchBox.sendKeys(keyword, Key.RETURN);
+      await driver.sleep(2000); // Introduce a 2-second delay
 
-    const searchResults = await driver.findElements(By.css('div.g'));
+      const searchResults = await driver.findElements(By.css('div.g'));
 
-    const results = [];
-    for (let i = 0; i < Math.min(searchResults.length, 10); i++) {
-      const result = await searchResults[i].findElement(By.css('a'));
-      const url = await result.getAttribute('href');
-      results.push(url);
+      const results = [];
+      for (let i = 0; i < Math.min(searchResults.length, 10); i++) {
+        const result = await searchResults[i].findElement(By.css('a'));
+        const url = await result.getAttribute('href');
+        results.push(url);
+      }
+
+      const data = JSON.stringify(results, null, 2);
+      fs.writeFileSync('search_results.json', data);
+      console.log('Test Case1 passed : results saved to search_results.json');
+
+    } catch(error) {
+      console.log("Error Occured")
     }
 
-    const data = JSON.stringify(results, null, 2);
-    fs.writeFileSync('search_results.json', data);
-    console.log('Search results saved to search_results.json');
+    //Test Case 2 : 1 link is found for given keyword
+    try {
+      let searchBox2 = await driver.findElement(By.name('q'));
+      await searchBox2.clear();
+      keyword = "dinkachi chav kay aste"
+      await searchBox2.sendKeys(keyword, Key.RETURN);
+      await driver.sleep(2000); // Introduce a 2-second delay
+
+      const searchResults = await driver.findElements(By.css('div.g'));
+
+      const results = [];
+      for (let i = 0; i < Math.min(searchResults.length, 10); i++) {
+        const result = await searchResults[i].findElement(By.css('a'));
+        const url = await result.getAttribute('href');
+        results.push(url);
+      }
+
+      const data = JSON.stringify(results, null, 2);
+      fs.writeFileSync('search_results2.json', data);
+      console.log('Test Case2 passed : result saved to search_results2.json');
+    } catch(error) {
+      console.log(error,"Error Occured")
+    }
+
+    //Test Case 3 : no found any link for given keyword
+
+    try {
+      let searchBox3 = await driver.findElement(By.name('q'));
+      await searchBox3.clear();
+      keyword = 'NoSuchElementError: no such element: Unable to locate element: {"method":"xpath","selector":"//*[@id="input"]"}(Session info: chrome=113.0.5672.127)at Object.throwDecodedError (C:\Users\I527305\Desktop\Bits\\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\error.js:524:15) at parseHttpResponse (C:\Users\I527305\Desktop\Bits\\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\http.js:601:13) at Executor.execute (C:\Users\I527305\Desktop\Bits\\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\http.js:529:28) at processTicksAndRejections (node:internal/process/task_queues:96:5) at async Driver.execute (C:\Users\I527305\Desktop\Bits\\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\webdriver.js:745:17) at async scrapeSearchResults (C:\Users\I527305\Desktop\Bits\\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\top10WebScrape.js:12:23)';
+      await searchBox3.sendKeys(keyword, Key.RETURN);
+      await driver.sleep(2000); // Introduce a 2-second delay
+
+      const searchResults = await driver.findElements(By.css('div.g'));
+
+      const results = [];
+      for (let i = 0; i < Math.min(searchResults.length, 10); i++) {
+        const result = await searchResults[i].findElement(By.css('a'));
+        const url = await result.getAttribute('href');
+        results.push(url);
+      }
+
+      const data = JSON.stringify(results, null, 2);
+      fs.writeFileSync('search_results3.json', data);
+      console.log('Test Case3 passed : results saved to search_results3.json');
+    } catch(error) {
+      console.log(error,"Error Occured")
+    }
+
   } catch(error) {
-    console.log("Exception - No result found!");
+    console.log(error,"Exception - No result found!");
   }
   finally {
     await driver.quit();
   }
 }
 
-// case 1: no found any link for given keyword
-// const keyword = 'NoSuchElementError: no such element: Unable to locate element: {"method":"xpath","selector":"//*[@id="input"]"}(Session info: chrome=113.0.5672.127)at Object.throwDecodedError (C:\Users\I527305\Desktop\Bits\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\error.js:524:15) at parseHttpResponse (C:\Users\I527305\Desktop\Bits\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\http.js:601:13) at Executor.execute (C:\Users\I527305\Desktop\Bits\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\http.js:529:28) at processTicksAndRejections (node:internal/process/task_queues:96:5) at async Driver.execute (C:\Users\I527305\Desktop\Bits\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\node_modules\selenium-webdriver\lib\webdriver.js:745:17) at async scrapeSearchResults (C:\Users\I527305\Desktop\Bits\2nd sem\Software Testing methodologies\STM Lab\Assignment1_WebScraping\top10WebScrape.js:12:23)';
-//case 2: 10 links are found for given keyword
-// const keyword = "cloud computing"
-//case 3: 1 link found for given keyword
-const keyword = 'dinkachi chav kay aste';
-scrapeSearchResults(keyword);
+
+//call the webscraping function
+scrapeSearchResults();
